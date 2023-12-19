dd0e20127f5d2726ab8dba55229e134fc1c85b7e
kcx <kuaichangxiu@outlook.com>
2023-12-19 17:29:31 +0800

    gitTest4

--- src/main/java/com/test/git/gitTest.java
+++ src/main/java/com/test/git/gitTest.java
@@ -2,8 +2,8 @@ package com.test.git;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.printf("hello git!");
-        System.out.printf("hello git!");
+        System.out.printf("hello git11!");
+        System.out.printf("hello git!22");
         System.out.printf("hello git!");
         System.out.printf("hello git!22");
         System.out.printf("hello git!333");

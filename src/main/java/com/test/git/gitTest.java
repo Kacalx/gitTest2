b5fcc22f8a662a9c7f6bfc2830323af17d630780
kcx <kuaichangxiu@outlook.com>
2023-12-19 17:22:05 +0800

    gitTest3

--- src/main/java/com/test/git/gitTest.java
+++ src/main/java/com/test/git/gitTest.java
@@ -6,7 +6,7 @@ public class gitTest {
         System.out.printf("hello git!");
         System.out.printf("hello git!");
         System.out.printf("hello git!22");
-        System.out.printf("hello git!");
+        System.out.printf("hello git!333");
         System.out.printf("hello git!");
         System.out.printf("hello git!");
     }

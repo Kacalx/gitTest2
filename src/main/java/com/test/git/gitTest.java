d1d487591910a007aa1b37410fa88b0527c371f0
kcx <kuaichangxiu@outlook.com>
2023-12-20 09:14:05 +0800

    master gitee1

--- src/main/java/com/test/git/gitTest.java
+++ src/main/java/com/test/git/gitTest.java
@@ -4,6 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.printf("hello git11!");
         System.out.printf("hello git!22");
-
+        System.out.printf("hello gitee!22");
     }
 }

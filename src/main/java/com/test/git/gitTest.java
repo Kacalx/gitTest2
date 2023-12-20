e880e3efd54c6bd4ee389970852786dc1b4b6ce9
kcx <kuaichangxiu@outlook.com>
2023-12-20 08:47:05 +0800

    master2

--- src/main/java/com/test/git/gitTest.java
+++ src/main/java/com/test/git/gitTest.java
@@ -3,5 +3,6 @@ package com.test.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.printf("master git1!");
+        System.out.printf("master git2!");
     }
 }

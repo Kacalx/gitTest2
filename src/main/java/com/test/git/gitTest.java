2ed4bf283041d9de32ff57a6e5caa20ebe70051f
kcx <kuaichangxiu@outlook.com>
2023-12-20 08:45:20 +0800

    master1

--- src/main/java/com/test/git/gitTest.java
+++ src/main/java/com/test/git/gitTest.java
@@ -2,21 +2,6 @@ package com.test.git;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.printf("hello git11!");
-        System.out.printf("hello git!22");
-        System.out.printf("hello git!1121");
-        System.out.printf("hello git!");
-        System.out.printf("hello git!");
-        System.out.printf("hello git!22");
-        System.out.printf("hello git!333");
-        System.out.printf("hello git!");
-        System.out.printf("hello git!");
-        System.out.printf("push git!");
-        System.out.printf("pull git!");
-        System.out.printf("pull git2!");
-        System.out.printf("gitee git!");
-         System.out.printf("gitee2 gitsdfsd!");
-        System.out.printf("pull git!sdfsdf");
-        System.out.printf("pull git2!dfgdf");
+        System.out.printf("master git1!");
     }
 }

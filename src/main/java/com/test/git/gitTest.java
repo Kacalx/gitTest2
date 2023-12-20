fa5d3d94df324892fade6fe099f3c7d8230979c9
kcx <kuaichangxiu@outlook.com>
2023-12-20 09:10:49 +0800

    master github1

--- src/main/java/com/test/git/gitTest.java
+++ src/main/java/com/test/git/gitTest.java
@@ -4,15 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.printf("hello git11!");
         System.out.printf("hello git!22");
-        System.out.printf("hello git!1121");
-        System.out.printf("hello git!");
-        System.out.printf("hello git!");
-        System.out.printf("hello git!22");
-        System.out.printf("hello git!333");
-        System.out.printf("hello git!");
-        System.out.printf("hello git!");
-        System.out.printf("push git!");
-        System.out.printf("pull git!");
-        System.out.printf("pull git2!");
+
     }
 }

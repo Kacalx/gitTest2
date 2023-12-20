a81fca2e4ccd0c01e64406cce3fbf4f975f4b1a1
kcx <kuaichangxiu@outlook.com>
2023-12-20 08:46:30 +0800

    hot1

--- src/main/java/com/test/git/gitTest.java
+++ src/main/java/com/test/git/gitTest.java
@@ -2,17 +2,6 @@ package com.test.git;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.printf("hello git11!");
-        System.out.printf("hello git!22");
-        System.out.printf("hello git!1121");
-        System.out.printf("hello git!");
-        System.out.printf("hello git!");
-        System.out.printf("hello git!22");
-        System.out.printf("hello git!333");
-        System.out.printf("hello git!");
-        System.out.printf("hello git!");
-        System.out.printf("push git!");
-        System.out.printf("pull git!sdfsdf");
-        System.out.printf("pull git2!dfgdf");
+        System.out.printf("hot git11!");
     }
 }
